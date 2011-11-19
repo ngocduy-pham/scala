33bf4a1b65517a51ff8f4bd318ff7db1a866f62b
ngocduy pham <de.sthy.university@gmail.com>
2011-11-19 22:03:58 +0700

    Added good ListBuffer_size.scala benchmark

--- benchmark/files/steady/ListBuffer_size.scala
+++ benchmark/files/steady/ListBuffer_size.scala
@@ -1,9 +1,26 @@
-object ListBuffer_size {
+/** scala/trunk/src/library/scala/collection/mutable/ListBuffer.scala
+ *  Bug:
+ *  {{{
+ *  // Nothing
+ *  }}}
+ *
+ *  Fix:
+ *  {{{
+ *  override def size = length
+ *  }}}
+ *
+ */
+object ListBuffer_size extends scala.tools.sbs.performance.PerformanceBenchmarkTemplate {
 
-  def main(args: Array[String]) {
-    val lb = collection.mutable.ListBuffer((0 to 1000000): _*)
-    val ls = List(lb.size)
+  override val measurement = 11
+
+  val lb = (for(_ <- 1 to 100000) yield collection.mutable.ListBuffer((0 to 15): _*)).toList
+
+  def init() = ()
+  def run() {
+    val ls = (1 to 15000) map (lb map (_ size))
     ls foreach (_ => ())
   }
+  def reset() = ()
 
 }
